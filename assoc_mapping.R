2596282f6b44b2c973d2aa59d0da7cbc4b0d062c
Daniel Gatti <Dan.Gatti@jax.org>
2017-08-18 10:38:21 -0400

    Fixed covariates with drop = FALSE.

--- assoc_mapping.R
+++ assoc_mapping.R
@@ -1,38 +1,52 @@
+###########
+# Libraries
 library(qtl2)
-library(RSQLite)
-library(dplyr)
+library(qtl2db)
+library(qtl2convert)
+################################################################################
 # Arguments:
-# probs: genoprobs object in qtl2 format.
+# probs: genoprobs object for one chromosome in qtl2 format.
 # pheno: matrix of phenotypes, samples in rows, phenotypes in columns.
 # idx: column index of the phenotype that you want to map.
 # addcovar: covariates matrix as used in scan1().
 # intcovar: covariate to interact with QTL.
-# k: list of kinship matrices.
+# K: kinship matrix for the current chromosome.
 # markers: data.frame containing 4 columns: marker, chr, bp, cM.
 # chr: Chromosome to map on.
 # start: start position for mapping in Mb.
 # end: end position for mapping in Mb.
 # ncores: number of cores to use in mappin.
-# db.file: Location of the mySQL database containing the Sanger SNPs.
-assoc_mapping = function(probs, pheno, idx, addcovar, intcovar = NULL, k, markers, 
-                chr, start, end, ncores, 
-                db.file = "/data/gac/resource/CCsnps/ccfoundersnps.sqlite") {
+# snp.file: Location of the mySQL database containing the Sanger SNPs. This
+#           was built by Karl and given to us.
+assoc_mapping = function(probs, pheno, idx, addcovar, intcovar = NULL, K, 
+                markers, chr, start, end, ncores = 1, 
+                snp.file = "cc_variants.sqlite") {
 
-  # Subset probs and K to keep only the current chromosome.
-  probs = probs[,chr]
-  k     = k[[chr]]
+  # Make sure that we have only one chromosome.
+  if(length(probs) > 1) {
+    stop(paste("Please provide a probs object with only the current chromosome."))
+  } # if(length(probs) > 1)
+
+  if(length(K) > 1) {
+    stop(paste("Please provide a kinship object for the current chromosome."))
+  } # if(length(K) > 1)
+
+  stopifnot(length(probs) == length(K))
+
+  # Create a function to query the SNPs.
+  query_variants = create_variant_query_func(snp.file)
+
+  # Convert marker positions to Mb if not already done.
+  # The longest mouse chromosome is ~200 Mb, so 300 should cover it.
+  if(max(markers[,3], na.rm = T) > 300) {
+    markers[,3] = markers[,3] * 1e-6
+  } # if(max(markers[,3], na.rm = T) > 300)
 
   # Split up markers into a vector of map positions.
-  map = split(markers[,3] * 1e-6, markers[,2])
-  nm  = split(markers[,1], markers[,2])
-  map = mapply(function(x, y) { names(x) = y;x }, map, nm)
-  map = map[order(as.numeric(names(map)))]
+  map = map_df_to_list(map = markers, pos_column = "pos")
 
   # Extract SNPs from the database
-  my_db = src_sqlite(db.file, create = FALSE)
-  snpinfo = tbl(my_db, sql(paste0("SELECT * FROM snps WHERE chr='", 
-            chr, "' AND pos_Mbp>=", start, " AND pos_Mbp<=", end))) %>%
-            collect(n = Inf)
+  snpinfo = query_variants(chr, start, end)
 
   # Names have to be replaced for future methods
   colnames(snpinfo)[c(1,3)] = c("snp", "pos")
@@ -40,18 +54,19 @@ assoc_mapping = function(probs, pheno, idx, addcovar, intcovar = NULL, k, marker
   # Index groups of similar SNPs.
   snpinfo = index_snps(map = map, snpinfo)
 
-  # Find which phenotype data actually exist
-  sel = !is.na(pheno[,idx])
+  # Keep samples that are not NA.
+  keep = !is.na(pheno[,idx])
 
   # Convert genoprobs to snpprobs.
-  snppr = genoprob_to_snpprob(probs[sel,], snpinfo)
+  snppr = genoprob_to_snpprob(probs[keep,], snpinfo)
   
   # Scan1.
-  assoc = scan1(pheno = pheno[sel,idx, drop = FALSE], kinship = k[sel,sel],
-          genoprobs = snppr, addcovar = addcovar[sel,], 
-          intcovar = addcovar[sel,intcovar], cores = ncores)
+  assoc = scan1(pheno = pheno[keep,idx, drop = FALSE], kinship = K[[1]][keep, keep],
+          genoprobs = snppr, addcovar = addcovar[keep,,drop=FALSE], 
+          intcovar = addcovar[keep,intcovar], cores = ncores)
 
   # Return the scan data.
   return(list(assoc, snpinfo))
 
 } # assoc_mapping()
+
